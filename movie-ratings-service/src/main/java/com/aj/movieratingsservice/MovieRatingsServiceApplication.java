aba237c4902f02b4b06c1b84f2943998c675ff40
AlanJosy <alan.j@inapp.com>
2022-02-08 12:31:29 +0530

    intial commit

---
+++ movie-ratings-service/src/main/java/com/aj/movieratingsservice/MovieRatingsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aj.movieratingsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRatingsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRatingsServiceApplication.class, args);
+	}
+
+}
